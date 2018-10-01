150bef890f0bde9b15acd73cf56124264f98f0b5
greeflas <vldmr.kuprienko@gmail.com>
2018-10-01 19:55:34 +0300

    Getters and setters

--- src/Person.php
+++ src/Person.php
@@ -5,4 +5,34 @@ class Person
     private $firstName;
     private $lastName;
     private $age;
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getAge()
+    {
+        return $this->age;
+    }
+
+    public function setAge($age)
+    {
+        $this->age = $age;
+    }
 }
